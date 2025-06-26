25c5324834daec667505eeb3dd36507de0130d34
weikang <weikangsu@live.co.uk>
2025-06-26 16:40:59 +0100

    bucket and cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions2228"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
